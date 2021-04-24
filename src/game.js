c3f22a5cf71801cada0f4babe6dd403f29175861
Danila Julev <daniljulev@gmail.com>
2021-04-25 01:24:39 +0300

    better way for rotating

--- src/game.js
+++ src/game.js
@@ -31,28 +31,10 @@ export default class Game {
     get blocks() {
       return this.rotations[this.rotationIndex];
     },
-    rotationIndex: 0,
-    rotations: [
-      [
-        [0,1,0],
-        [1,1,1],
-        [0,0,0],
-      ],
-      [
-        [0,1,0],
-        [0,1,1],
-        [0,1,0],
-      ],
-      [
-        [0,0,0],
-        [1,1,1],
-        [0,1,0],
-      ],
-      [
-        [0,1,0],
-        [1,1,0],
-        [0,1,0],
-      ],
+    blocks: [
+      [0,1,0],
+      [1,1,1],
+      [0,0,0],
     ],
   };
 
@@ -82,13 +64,36 @@ export default class Game {
   }
 
   rotatePiece() {
-    this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0;
-    
+    this.rotateBlocks();
     if (this.hasCollision()) {
-      this.activePiece.rotationIndex = this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3;
+      this.rotateBlocks(false);
+    }
+  }
+
+  rotateBlocks(clockwise = true) {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    const x = Math.floor(length / 2);
+    const y = length - 1;
+
+    for (let i = 0; i < x; i++) {
+      for (let j = i; j < y - i; j++) {
+        const temp = blocks[i][j];
+        
+        if (clockwise) {
+          blocks[i][j]          = blocks[y - j][i];
+          blocks[y - j][i]      = blocks[y - i][y - j];
+          blocks[y - i][y - j]  = blocks[j][y - i];
+          blocks[j][y - i]      = temp;
+        } else { 
+          blocks[i][j]          = blocks[j][y - i];
+          blocks[j][y - i]      = blocks[y - i][y - j];
+          blocks[y - i][y - j]  = blocks[y - j][i];
+          blocks[y - j][i]      = temp;
+        }
+      } 
     }
 
-    return this.activePiece.blocks;
   }
 
   hasCollision() {
